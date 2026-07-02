687c019df5db0994b64672c68e8931a3c1c41a26
tsenguun <tsenguunbattsengel@G303-CC0152.local>
2026-07-02 10:06:28 +0800

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,22 @@ import { Footer } from "@/components/Footer";
 import Nav from "@/components/Nav";
 import { GroupMovie } from "@/components/GroupMovie";
 
-// ...interface movieType
+export interface movieType {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export default function Home() {
   return (
